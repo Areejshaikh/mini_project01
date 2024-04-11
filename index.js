9047fde86ee47e68084c7548d6e00e53c1093e7f
Areejshaikh <160318564+Areejshaikh@users.noreply.github.com>
2024-04-12 00:08:19 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+"use strict";
+// Q1: Write a program that checks whether the given number is positive or negative.
+// let num : number = 10; //  lets check this number
+//  if (num > 0) { // if a number is positive.
+//     console.log("Number is positive !");  // number is greater than 0 means number is positive
+//  }  
+//  else { //number is not greater than 0 means number is Nagative
+//      console.log("Number is Nagative !")   
+//  }
+// lets cheak another Example 
+let num1 = -10; //  lets check this number
+if (num1 < 0) { // if a number Nagative.
+    console.log("Number is Nagative !"); // number is less than 0 means number is nagative
+}
+else { //number is not greater than 0 means number is Nagative
+    console.log("Number is positive !");
+}
